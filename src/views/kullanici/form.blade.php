5377d12abe9220dbe4ce4bb028d7eb1ba9960109
ayzamodul <57704718+ayzamodul@users.noreply.github.com>
2019-12-06 10:39:12 +0300

    Update form.blade.php

--- src/views/kullanici/form.blade.php
+++ src/views/kullanici/form.blade.php
@@ -1,50 +1,58 @@
 @extends('yonetim.layouts.master')
 @section('title','Kullanıcı Yönetimi')
 @section('content')
-    <h1 class="page-header">Kullanıcı Yönetimi</h1>
+    <section>
+
+        <div class="card" style="background-color: white" id="deneme">
 
 
-    <form method="post" action="{{route('yonetim.kullanici.kaydet', @$entry->id)}}">
+                <form method="post" action="{{route('yonetim.kullanici.kaydet', @$entry->id)}}">
 
-        {{csrf_field()}}
-
-        <div class="pull-right">
-            <button type="submit" class="btn btn-primary">
-                {{@$entry->id > 0 ? "Güncelle" : "Kaydet"}}
-            </button>
-        </div>
-        <h2 class="sub-header">
-            Kullanıcı {{@$entry->id > 0 ? "Düzenle" : "Ekle"}}
-        </h2>
+                    {{csrf_field()}}
+                    <div class="card-header d-flex align-items-center">
+                        <h3 class="h4">Kullanıcı {{@$entry->id > 0 ? "Düzenle" : "Ekle"}}</h3>
+                    </div>
 
 
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="adsoyad">Ad</label>
-                    <input type="text" class="form-control" id="name" name="name" placeholder=""
-                           value="{{$entry->adsoyad}}">
-                </div>
+<div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="adsoyad">Ad</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder=""
+                                       value="{{$entry->name}}">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email</label>
+                                <input type="email" class="form-control" id="email" name="email"
+                                       value="{{$entry->email}}">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="sifre">Şifre</label>
+                                <input type="password" class="form-control" id="sifre" name="sifre" placeholder="Şifre"
+                                       value="{{$entry->password}}">
+                            </div>
+                        </div>
+                    </div>
+</div>
+                    <div class="pull-right">
+                        <button type="submit" class="btn btn-primary">
+                            {{@$entry->id > 0 ? "Güncelle" : "Kaydet"}}
+                        </button>
+                    </div>
+                </form>
+                <br>
+                <hr>
             </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" value="{{$entry->email}}">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="sifre">Şifre</label>
-                    <input type="password" class="form-control" id="sifre" name="sifre" placeholder="Şifre"
-                           value="{{$entry->password}}">
-                </div>
-            </div>
-        </div>
-    </form>
-
+    </section>
 @endsection
